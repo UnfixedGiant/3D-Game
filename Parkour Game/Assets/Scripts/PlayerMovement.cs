194ce55899ad129d795b78a1ceae6627a33a051f
UnfixedGiant <80041951+UnfixedGiant@users.noreply.github.com>
2025-03-03 15:06:14 +0000

    Player Movement
    Added some basic player movement

---
+++ Parkour Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables.
+    public float moveSpeed;
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+
+    // Methods.
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Update () 
+    {
+        MyInput();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        // Movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+}
