ed51891fa2cc7fc30bc859c072d5a092b42d1622
UnfixedGiant <80041951+UnfixedGiant@users.noreply.github.com>
2025-05-14 17:32:00 +0100

    Enemy Patrolling
    Simple Patrolling enemy that follows a path and stops for 3 seconds every time it reaches a point in the path.

--- Parkour Game/Assets/Scripts/Enemy/States/StateMachine.cs
+++ Parkour Game/Assets/Scripts/Enemy/States/StateMachine.cs
@@ -6,6 +6,13 @@ public class StateMachine : MonoBehaviour
 {
 
     public BaseState activeState;
+    public PatrolState patrolState;
+
+    public void Initialise()
+    {
+        patrolState = new PatrolState();
+        changeState(patrolState);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +40,7 @@ public class StateMachine : MonoBehaviour
         if (activeState != null)
         {
             activeState.stateMachine = this;
+            activeState.enemy = GetComponent<Enemy>();
             activeState.Enter();
         }
     }
