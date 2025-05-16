f75295052150433a6c16ee2085f6f4a1aefd6cf1
UnfixedGiant <80041951+UnfixedGiant@users.noreply.github.com>
2025-05-16 10:52:26 +0100

    Added total coins collected in the end screen

--- Parkour Game/Assets/Scripts/EndGame.cs
+++ Parkour Game/Assets/Scripts/EndGame.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class EndGame : MonoBehaviour
 {
+    public TextMeshProUGUI coins;
+    void OnEnable()
+    {
+        coins.text = "Coins collected: " + Player.totalCoins.ToString();
+    }
     public void RestartGame()
     {
         SceneManager.LoadScene("Main Menu");
